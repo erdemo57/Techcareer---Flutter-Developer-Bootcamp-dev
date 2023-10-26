9ad0eb478cc1e7f246b7235fd540ddf967a68f06
Erdem ÖZTÜRK <58476258+erdemo57@users.noreply.github.com>
2023-10-26 16:23:52 +0300

    Add files via upload

---
+++ odev1.dart
@@ -0,0 +1,135 @@
+void main() {
+  String ilce = "İstanbul";
+  String kita = "Avrupa";
+  String faks = "1234567890";
+  String postaKodu = "34000";
+  String instaAdresi = "instagram.com/erdemozturk";
+  String calistiginBolum = "Yazılım Geliştirme";
+
+
+  print("ilçe : $ilce");
+  print("kıta  : $kita");
+  print("faks : $faks");
+  print("postaKodu : $postaKodu");
+  print("İnstagram Adresi : $instaAdresi");
+  print("Çalistigin Bolum : $calistiginBolum");
+
+
+
+
+  int urunMiktari = 100;
+  String musteriSoyadi = "Örnek";
+  double odemeMiktari = 250.50;
+  DateTime dogumTarihi = DateTime(1990, 5, 15);
+  double borc = 1000.0;
+  String medeniHal = "Bekar";
+  String videoYorumu = "Harika bir video!";
+  String odemeSaati = "14:30";
+  double eftMiktari = 350.75;
+  int satilanMiktar = 50;
+  String telefonModeli = "Samsung Galaxy S21";
+
+  print("urun Miktari : $urunMiktari");
+  print("musteri Soyadi : $musteriSoyadi");
+  print("odeme Miktari : $odemeMiktari");
+  print("dogum Tarihi : $dogumTarihi");
+  print("borc : $borc");
+  print("medeniHal : $medeniHal");
+  print("videoYorumu : $videoYorumu");
+  print("odemeSaati : $odemeSaati");
+  print("eftMiktari : $eftMiktari");
+  print("satilanMiktar : $satilanMiktar");
+  print("telefonModeli : $telefonModeli");
+
+
+
+
+
+
+
+
+
+  String dergiAdi = "Newbox";
+  String basimTarihi = "01/10/2023";
+  double zamMiktari = 2.50;
+  int daireSayisi = 100;
+  double enlem = 41.0;
+  double boylam = 29.0;
+  String yemekAdi = "Pizza";
+  double urunFiyati = 20.0;
+
+  print("dergiAdi : $dergiAdi");
+  print("basimTarihi : $basimTarihi");
+  print("zamMiktari  : $zamMiktari");
+  print("daireSayisi : $daireSayisi");
+  print("enlem : $enlem");
+  print("boylam : $boylam");
+  print("yemekAdi : $yemekAdi");
+  print("urunFiyati : $urunFiyati");
+
+
+
+
+  String sirket = "ABC Şirketi";
+  print("Şirket İsi : $sirket");
+
+
+  String videoAdi = "Flutter Tanıtımı";
+  int muzikSuresi = 120;
+  double mekanPuani = 4.5;
+
+  print("videoAdi : $videoAdi");
+  print("muzikSuresi : $muzikSuresi");
+  print("mekanPuani : $mekanPuani");
+
+
+
+  String dosyaAdi = "Belge.pdf";
+  String resimFormati = "JPEG";
+  String renk = "Mavi";
+  String renkKodu = "#0000FF";
+  String bilgisayarModeli = "HP Envy";
+  String ekranBoyutu = "15.6 inç";
+  String kullanimSuresi = "2 yıl";
+
+
+  print("dosyaAdi : $dosyaAdi");
+  print("resimFormati : $resimFormati");
+  print("renk : $renk");
+  print("renkKodu : $renkKodu");
+  print("bilgisayarModeli : $bilgisayarModeli");
+  print("ekranBoyutu : $ekranBoyutu");
+  print("kullanimSuresi : $kullanimSuresi");
+
+
+
+
+
+  double basinc = 100.5;
+  String etkinlikGunu = "Pazar";
+  String odemeGunu = "Cuma";
+  DateTime yolculukCikisTarihi = DateTime(2023, 11, 15);
+  String otobusHatti = "Hattı A";
+  int kullanilanDakika = 120;
+  String kargoTakipNumarasi = "ABC12345";
+  int kuponSuresi = 30;
+  String kuponKodu = "KUPON123";
+  DateTime faturaTarihi = DateTime(2023, 10, 26);
+
+  print("basinc : $basinc");
+  print("etkinlikGunu : $etkinlikGunu");
+  print("odemeGunu : $odemeGunu");
+  print("yolculukCikisTarihi : $yolculukCikisTarihi");
+  print("otobusHatti : $otobusHatti");
+  print("kullanilanDakika : $kullanilanDakika");
+  print("kargoTakipNumarasi : $kargoTakipNumarasi");
+  print("kuponSuresi : $kuponSuresi");
+  print("kuponKodu : $kuponKodu");
+  print("faturaTarihi : $faturaTarihi");
+
+
+
+
+
+
+}
